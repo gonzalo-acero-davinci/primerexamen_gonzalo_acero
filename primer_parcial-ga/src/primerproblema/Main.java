24a81f24fb97d60a7c62b8709c29ce5957e88a2e
Gonzalo Acero <gonzalo.acero@davinci.edu.ar>
2021-10-27 17:15:18 -0300

    Primer parcial. Gonzalo Acero

--- primer_parcial-ga/src/primerproblema/Main.java
+++ primer_parcial-ga/src/primerproblema/Main.java
@@ -3,10 +3,20 @@ package primerproblema;
 public class Main {
     public static void main(String[] args) {
         Ortoedro ortoedro = new Ortoedro();
+        ortoedro.calcularVolumen();
+        ortoedro.calcularSuperficie();
+        ortoedro.toString();
 
-        TetraedroRegular tetraedroRegular = new TetraedroRegular();
+        TetraedroRegular tetraedroregular = new TetraedroRegular();
+        tetraedroregular.calcularSuperficie();
+        tetraedroregular.calcularVolumen();
+        tetraedroregular.toString();
+
 
         Cubo cubo = new Cubo();
+        cubo.calcularSuperficie();
+        cubo.calcularVolumen();
+        cubo.toString();
 
 
 
